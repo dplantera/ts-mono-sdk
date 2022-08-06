166fbd0012ac01222415500a9c4e1913c4285416
danie <daniele.plantera@outlook.de>
2022-08-06 10:48:06 +0200

    some fixes

--- packages/create-package-mono/src/index.ts
+++ packages/create-package-mono/src/index.ts
@@ -1,7 +1,7 @@
 #!/usr/bin/env node
 
 import { Command } from 'commander';
-import { createMonoPackage } from './createMonoPackage';
+import { createMonoPackage } from './create-mono-package';
 
 export type CliOptions = {
   verbose: boolean;
